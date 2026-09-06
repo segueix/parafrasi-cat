fe2607432cb76385216dea6e515cbcbd6884cb36
Segueix <danielmasdexaxars@gmail.com>
2026-09-06 15:03:06 +0200

    Bump package version to 1.3.16

--- src/parafrasi_cat/__init__.py
+++ src/parafrasi_cat/__init__.py
@@ -19,7 +19,7 @@ from parafrasi_cat.pipeline.pipeline import Pipeline
 from parafrasi_cat.pipeline.result import ParaphraseResult
 from parafrasi_cat.protected.spans import ProtectedSpan, ProtectionKind
 
-__version__ = "1.3.15"
+__version__ = "1.3.16"
 
 __all__ = [
     "ParaphraseResult",
